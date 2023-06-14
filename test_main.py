a8b1e06df611c1a9b53ad6ed4748f40be059c4ec
liha4 <liha9122468689@gmail.com>
2023-06-14 12:46:22 +0500

    Добавлены комментарии к функциям, удалены закомментированные строки

--- test_main.py
+++ test_main.py
@@ -5,20 +5,27 @@ client = TestClient(app)
 
 
 def test_read_main():
+    '''
+    Функция тестирует доступ к проекту в сети и получение ответа с главной страницы
+    '''
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "summarization app. Use method /summarization"}
 
 
 def test_translate_get():
+    '''
+    Функция тестирует доступ к проекту в сети и получение ответа со страницы /summarization
+    '''
     response = client.get("/summarization")
     assert response.status_code == 200
     assert "text/html" in response.headers["content-type"]
 
 
 def test_handle_form():
+    '''
+    Функция тестирует доступ к проекту в сети и получение ответа при отправке данных формы
+    '''
     response = client.post("/summarization", data={"assignment": "Sample text"})
     assert response.status_code == 200
     assert "text/html" in response.headers["content-type"]
-    # assert "Sample text" in response.text
-    # assert "result" in response.text
